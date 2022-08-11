908cd84f8b240a4de2dc80d440107ffae153cf7d
Neo Mathekga <neozagm@gmail.com>
2022-08-11 07:45:28 -0700

    printf edit

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
- * Return: Printed chars..
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
@@ -52,10 +52,10 @@ int _printf(const char *format, ...)
 	return (printed_chars);
 }
 
-/**
+/*i*
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_ind: Index at which to add next char, represents the length
  */
 void print_buffer(char buffer[], int *buff_ind)
 {

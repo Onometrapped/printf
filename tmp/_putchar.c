0e2f872b49c9987aa4677d257be83da4f7e0549c
Neo Mathekga <neozagm@gmail.com>
2022-08-11 07:40:40 -0700

    edited putchar

--- tmp/_putchar.c
+++ tmp/_putchar.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The char to print
+ *_putchar - outputs char to stdout
+ *@c: the char to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *Return: on success 1
+ *
  */
 int _putchar(char c)
 {
